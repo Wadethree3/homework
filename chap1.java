5175141fc996d17b9fb8f6f0862b42481a9d2f6e
Wadethree3 <43160784+Wadethree3@users.noreply.github.com>
2018-09-20 16:06:37 +0800

    Add files via upload

---
+++ chap1.java
@@ -0,0 +1,20 @@
+package qwerty;
+
+public class chap1 {
+public static void zool(int age,String b,String c) {
+	System.out.println(age);
+	System.out.println(b);
+	System.out.println(c);
+}
+public static void america(String day,String date,int month,int year){
+	System.out.println(day);
+	System.out.println(date);
+	System.out.println(month);
+	System.out.println(year);
+}
+public static void main(String[]args) {
+	zool(11,"pet","qqq");
+	america("Saturday","July",11,2015);
+}
+
+}
